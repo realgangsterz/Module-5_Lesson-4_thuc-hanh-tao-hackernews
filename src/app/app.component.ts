71ca43597503249217a2d3ce1547afec9cf8fcd7
daoxuandong1997 <daoxuandong1997@gmail.con>
2020-09-24 14:26:42 +0700

    Không chạy function addArticle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'news-hacker';
+  article = {
+    title: 'The Evolution of Async JavaScript: From Callbacks, to Promises, to Async/Await',
+    url: 'https://medium.freecodecamp.org/the-evolution-of-async-javascript-from-callbacks-to-promises-to-async-await-e73b047f2f40'
+  };
+
+  articles = [
+    {
+      title: 'The Evolution of Async JavaScript: From Callbacks, to Promises, to Async/Await',
+      url: 'https://medium.freecodecamp.org/the-evolution-of-async-javascript-from-callbacks-to-promises-to-async-await-e73b047f2f40'
+    },
+    {
+      title: 'Game of Life',
+      url: 'https://thefullsnack.com/posts/game-of-life.html'
+    },
+    {
+      title: 'Nguyên tắc thiết kế REST API',
+      url: 'https://medium.com/eway/nguyên-tắc-thiết-kế-rest-api-23add16968d7'
+    },
+    {
+      title: 'Why You Only Need to Test with 5 Users',
+      url: 'https://www.nngroup.com/articles/why-you-only-need-to-test-with-5-users/'
+    },
+    {
+      title: 'Let’s Build A Web Server. Part 1.',
+      url: 'https://ruslanspivak.com/lsbaws-part1/'
+    }
+  ];
+
+  addArticle() {
+    this.article.title = (<HTMLInputElement> document.getElementById('article-title')).value;
+    this.article.url = (<HTMLInputElement> document.getElementById('article-url')).value;
+    this.articles.push(this.article);
+  }
 }
